b7648d23b77f28bc4d487b87329fe2c5ac3ff0af
zee-creato <85754092+zee-creato@users.noreply.github.com>
2023-02-03 09:06:25 +0000

    first commit

---
+++ grpc-demo-main/gRPC.Web.Server/StreamImplService.cs
@@ -0,0 +1,32 @@
+
+using Grpc.Core;
+using Microsoft.AspNetCore.Authorization;
+
+public class StreamImplService: StreamService.StreamServiceBase {
+    
+    private readonly List<string> _messages = new List<string>()
+    {
+      "Hello",
+      "World",
+      "!"
+    };
+    
+    public override async Task FetchResponse(
+        Request request, 
+        IServerStreamWriter<Response> responseStream, 
+        ServerCallContext context)
+    {
+        while (!context.CancellationToken.IsCancellationRequested)
+        {
+            foreach (var message in _messages)
+            {
+                await responseStream.WriteAsync(new Response()
+                {
+                    Result = message
+                });
+
+                Thread.Sleep(750);
+            }
+        }
+    }
+}
